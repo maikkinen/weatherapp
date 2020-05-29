069c17cbe1ba5a43aff0b4b14f1aca6ab5ec3321
unknown <anna-maija.rauramaa@aalto.fi>
2020-05-29 12:53:55 +0300

    Android app Hello World

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Hello World! Hello everyone!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="Button" onPress={ () => setText('Hello Mars!') }/>
     </View>
   );
 }
